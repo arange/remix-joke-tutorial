3ef71f8fb544ae01d9aaeb7e3b570a9e961071d7
Pan Zhan <pan.zhan@educationhorizons.com>
2022-02-11 01:34:58 +1100

    url params

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,11 +1,22 @@
+import type { Joke } from "@prisma/client";
+import { LoaderFunction, useLoaderData } from "remix";
+import { db } from "~/utils/db.server";
+
+export const loader: LoaderFunction = async ({ params }) => {
+  console.log(params);
+  const joke = await db.joke.findUnique({
+    where: { id: params.jokeId },
+  });
+  if (!joke) throw new Error("Joke not found");
+  return joke;
+};
+
 export default function JokeRoute() {
+  const joke = useLoaderData<Joke>();
   return (
     <div>
       <p>Here's your hilarious joke:</p>
-      <p>
-        Why don't you find hippopotamuses hiding in trees?
-        They're really good at it.
-      </p>
+      <p>{joke.content}</p>
     </div>
   );
 }
