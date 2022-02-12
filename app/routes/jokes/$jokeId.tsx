1f485f183e5cd59dd996c24aedbfaac524e6f93e
Pan Zhan <pan.zhan@educationhorizons.com>
2022-02-12 22:28:36 +1100

    Add & remove joke

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,6 +1,14 @@
 import type { Joke } from "@prisma/client";
-import { LoaderFunction, useLoaderData } from "remix";
+import { ActionFunction, LoaderFunction, redirect, useLoaderData } from "remix";
 import { db } from "~/utils/db.server";
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+  const id = form.get("id");
+  if (typeof id !== "string") throw Error("Invalid id");
+  await db.joke.delete({ where: { id } });
+  return redirect("/jokes");
+};
 
 export const loader: LoaderFunction = async ({ params }) => {
   console.log(params);
@@ -17,6 +25,12 @@ export default function JokeRoute() {
     <div>
       <p>Here's your hilarious joke:</p>
       <p>{joke.content}</p>
+      <form method="post">
+        <input type="hidden" name="id" value={joke.id} readOnly />
+        <button className="button" type="submit">
+          Delete
+        </button>
+      </form>
     </div>
   );
 }
--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,24 +1,43 @@
-import { Form } from "remix";
+import type { ActionFunction } from "remix";
+import { redirect } from "remix";
+
+import { db } from "~/utils/db.server";
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+  const name = form.get("name");
+  const content = form.get("content");
+  // we do this type check to be extra sure and to make TypeScript happy
+  // we'll explore validation next!
+  if (typeof name !== "string" || typeof content !== "string") {
+    throw new Error(`Form not submitted correctly.`);
+  }
+
+  const fields = { name, content };
+
+  const joke = await db.joke.create({ data: fields });
+  return redirect(`/jokes/${joke.id}`);
+};
 
 const NewJokeRoute = () => {
   return (
     <div>
       <p>Add your new hilarious joke</p>
-      <Form>
+      <form method="post">
         <div>
           <label>
-            Name
-            <input placeholder="Name of the joke"></input>
+            Name:
+            <input type="text" name="name" placeholder="Name of the joke"></input>
           </label>
         </div>
         <div>
           <label>
-            Content
-            <textarea placeholder="Content of the joke"></textarea>
+            Content:
+            <textarea name="content" placeholder="Content of the joke"></textarea>
           </label>
         </div>
-        <button type="submit">Add</button>
-      </Form>
+        <button className="button" type="submit">Add</button>
+      </form>
     </div>
   );
 };
